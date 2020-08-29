aad9710e9a342b10d6daa0e8196cf30ea304e0c3
Pat Wangrungarun <phatograph@gmail.com>
2020-08-29 09:59:22 +0700

    rewrites

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   basePath: process.env.NODE_ENV == 'production' ? '/mai' : '',
   assetPrefix: process.env.NODE_ENV == 'production' ? '/mai/' : '',
-  trailingSlash: true,
+  rewrites: async () => {
+    return [
+      { source: '/docs/_next/:path*', destination: '/_next/:path*' }
+    ]
+  },
+  // trailingSlash: true,
   // exportPathMap: async (
   //   defaultPathMap,
   //   {dev, dir, outDir, distDir, buildId}
