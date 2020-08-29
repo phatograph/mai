b5b5bb67726c71a87331dfe3602690c8ff43930b
Pat Wangrungarun <phatograph@gmail.com>
2020-08-29 09:54:13 +0700

    U

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   basePath: process.env.NODE_ENV == 'production' ? '/mai' : '',
-  assetPrefix: process.env.NODE_ENV == 'production' ? 'https://phatograph.github.io/mai/' : '',
-  // exportTrailingSlash: true,
+  assetPrefix: process.env.NODE_ENV == 'production' ? '/mai/' : '',
+  trailingSlash: true,
   // exportPathMap: async (
   //   defaultPathMap,
   //   {dev, dir, outDir, distDir, buildId}
