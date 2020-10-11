1f9892ecd1f653aa36e14b5fee0d46414387e5d1
Pat Wangrungarun <phatograph@gmail.com>
2020-10-11 17:51:46 +0700

    F

--- pages/index.js
+++ pages/index.js
@@ -10,53 +10,61 @@ import epl_fixture_20202021 from '@csvs/epl_fixture_20202021.csv'
 import epl_fixture_records from '@csvs/epl_fixture_records.csv'
 
 const Index = (props) => {
+  const f = format('.3f')
+
   const team = 'Newcastle Utd'
-  const fixtures = filter(epl_fixture_20202021, (x) => {
-    return get(x, 'away_team') == team || get(x, 'home_team') == team
-  })
 
   const fixturesRecords = filter(epl_fixture_records, (x) => {
     return get(x, 'away_team') == team || get(x, 'home_team') == team
   })
 
-  const f = format('.3f')
+  const fixtures = filter(epl_fixture_20202021, (x) => {
+    return get(x, 'away_team') == team || get(x, 'home_team') == team
+  }).map((x) => {
+    const [opponent, isAway] =
+      get(x, 'home_team') == team
+        ? [get(x, 'away_team'), false]
+        : [get(x, 'home_team'), true]
+
+    const opponentGames = filter(fixturesRecords, (y) =>
+      isAway ? get(y, 'home_team') == opponent : get(y, 'away_team') == opponent
+    )
+
+    const points = opponentGames.map((y, j) => {
+      let score = 0
+
+      if (
+        get(y, 'home_team') == team &&
+        get(y, 'score_home') > get(y, 'score_away')
+      ) {
+        score = 3
+      } else if (
+        get(y, 'away_team') == team &&
+        get(y, 'score_home') < get(y, 'score_away')
+      ) {
+        score = 3
+      } else if (get(y, 'score_home') == get(y, 'score_away')) {
+        score = 1
+      }
+
+      return score
+    })
+
+    return {
+      ...x,
+      _opponent: opponent,
+      _isAway: isAway,
+      _opponentGames: opponentGames,
+      _points: points,
+      _pointsAvg: f(sum(points) / get(opponentGames, 'length')),
+    }
+  })
 
   return (
     <div className='Index'>
       <div className='container'>
         <div className='Index__games'>
           {fixtures.map((x, i) => {
-            const [opponent, isAway] =
-              get(x, 'home_team') == team
-                ? [get(x, 'away_team'), false]
-                : [get(x, 'home_team'), true]
-
-            const opponentGames = filter(fixturesRecords, (y) =>
-              isAway
-                ? get(y, 'home_team') == opponent
-                : get(y, 'away_team') == opponent
-            )
-
-            const points = opponentGames.map((y, j) => {
-              let score = 0
-
-              if (
-                get(y, 'home_team') == team &&
-                get(y, 'score_home') > get(y, 'score_away')
-              ) {
-                score = 3
-              } else if (
-                get(y, 'away_team') == team &&
-                get(y, 'score_home') < get(y, 'score_away')
-              ) {
-                score = 3
-              } else if (get(y, 'score_home') == get(y, 'score_away')) {
-                score = 1
-              }
-
-              return score
-            })
-
             return (
               <div className='Index__game' key={i}>
                 <h2 className='Index__game__h2'>
@@ -65,14 +73,14 @@ const Index = (props) => {
                 </h2>
 
                 <p>
-                  {get(opponentGames, 'length')} games,{' '}
-                  {f(sum(points) / get(opponentGames, 'length'))} points
+                  {get(get(x, 'opponentGames'), 'length')} games,{' '}
+                  {get(x, '_pointsAvg')} points
                 </p>
 
                 {true && (
                   <React.Fragment>
                     <dl className='Index__game__dl'>
-                      {opponentGames.map((y, j) => {
+                      {get(x, '_opponentGames').map((y, j) => {
                         let score = 0
 
                         if (
