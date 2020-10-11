b5e6e065992b5f7ebd9d88fba7a47928bc0e1f4d
Pat Wangrungarun <phatograph@gmail.com>
2020-10-11 17:32:03 +0700

    Points

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,106 @@
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import PropTypes from 'prop-types'
-import {get, tail, takeRight, find} from 'lodash'
+import {get, tail, take, find, filter, sum} from 'lodash'
 import className from 'classnames'
 import axios from 'axios'
+import {format} from 'd3-format'
 
 import epl_fixture_20202021 from '@csvs/epl_fixture_20202021.csv'
-// import epl_fixture_records from '@csvs/epl_fixture_records.csv'
+import epl_fixture_records from '@csvs/epl_fixture_records.csv'
 
 const Index = (props) => {
-  const fixtures = epl_fixture_20202021.filter((x) => {
-    return (
-      get(x, 'away_team') == 'Newcastle Utd' ||
-      get(x, 'home_team') == 'Newcastle Utd'
-    )
+  const team = 'Newcastle Utd'
+  const fixtures = filter(epl_fixture_20202021, (x) => {
+    return get(x, 'away_team') == team || get(x, 'home_team') == team
   })
+
+  const fixturesRecords = filter(epl_fixture_records, (x) => {
+    return get(x, 'away_team') == team || get(x, 'home_team') == team
+  })
+
+  const f = format('.3f')
 
   return (
     <div className='Index'>
       <div className='container'>
-        {fixtures.map((x, i) => (
-          <div className='Index__game' key={i}>
-            <h2 className='Index__game__h2'>
-              {get(x, 'game_date')}: {get(x, 'home_team')} vs{' '}
-              {get(x, 'away_team')}
-            </h2>
-          </div>
-        ))}
+        {fixtures.map((x, i) => {
+          const opponent =
+            get(x, 'home_team') == team
+              ? get(x, 'away_team')
+              : get(x, 'home_team')
+
+          const opponentGames = filter(
+            fixturesRecords,
+            (y) =>
+              get(y, 'home_team') == opponent || get(y, 'away_team') == opponent
+          )
+
+          const points = opponentGames.map((y, j) => {
+            let score = 0
+
+            if (
+              get(y, 'home_team') == team &&
+              get(y, 'score_home') > get(y, 'score_away')
+            ) {
+              score = 3
+            } else if (
+              get(y, 'away_team') == team &&
+              get(y, 'score_home') < get(y, 'score_away')
+            ) {
+              score = 3
+            } else if (get(y, 'score_home') == get(y, 'score_away')) {
+              score = 1
+            }
+
+            return score
+          })
+
+          return (
+            <div className='Index__game' key={i}>
+              <h2 className='Index__game__h2'>
+                {get(x, 'game_date')}: {get(x, 'home_team')} vs{' '}
+                {get(x, 'away_team')}
+              </h2>
+
+              <p>
+                {get(opponentGames, 'length')} games,{' '}
+                {f(sum(points) / get(opponentGames, 'length'))} points
+              </p>
+
+              {false && (
+                <React.Fragment>
+                  <dl className='Index__game__dl'>
+                    {opponentGames.map((y, j) => {
+                      let score = 0
+
+                      if (
+                        get(y, 'home_team') == team &&
+                        get(y, 'score_home') > get(y, 'score_away')
+                      ) {
+                        score = 3
+                      } else if (
+                        get(y, 'away_team') == team &&
+                        get(y, 'score_home') < get(y, 'score_away')
+                      ) {
+                        score = 3
+                      } else if (get(y, 'score_home') == get(y, 'score_away')) {
+                        score = 1
+                      }
+
+                      return (
+                        <div className='Index__game__dl__item' key={j}>
+                          {get(y, 'home_team')} {get(y, 'score_home')} -{' '}
+                          {get(y, 'score_away')} {get(y, 'away_team')} = {score}
+                        </div>
+                      )
+                    })}
+                  </dl>
+                </React.Fragment>
+              )}
+            </div>
+          )
+        })}
       </div>
     </div>
   )
